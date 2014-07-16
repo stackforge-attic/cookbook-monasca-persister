34cb96a4bbf67e18c0a3c44cbae7aaf7673898bd
Craig Bryant <craig.bryant@hp.com>
2014-07-16 17:59:18 -0600

    Get initial build to work.
    Add .gitreview, Gemfile, .rubocop.yml, Berksfile, Strainerfile
    
    Fixed style errors.
    
    Added empty unit test so chef-unit passes until we get real unit tests
    
    Change-Id: I7e34f46e56e14aa9897b7f7fcad545e9c723412c

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
+# encoding: UTF-8#
+#
 node.default[:mon_persister][:group] = 'persister'
 node.default[:mon_persister][:data_bag] = 'mon_persister'
 
-#Sysctl settings
-#This style is picked up by the sysctl cookbook in HP Cloud basenode
+# Sysctl settings
+# This style is picked up by the sysctl cookbook in HP Cloud basenode
 node.default[:sysctl]['net.core.rmem_max'] = '16777216'
-#This style is used by the sysctl community cookbook
+# This style is used by the sysctl community cookbook
 node.default[:sysctl][:params][:net][:core][:rmem_max] = '16777216'
