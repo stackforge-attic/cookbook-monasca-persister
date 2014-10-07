9303d8be6dec70771254f3d80d3ec45f4f214e93
Tim Kuhlman <tim.kuhlman@hp.com>
2014-10-07 08:57:36 -0600

    Added sysctl as a dependency
    Change-Id: I99fd03a2acd84fd4cb5f1c6812397258f5d84b4f

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ maintainer_email 'monasca@lists.launchpad.net'
 license          'All rights reserved'
 description      'Installs/Configures som_persister'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.19'
+version          '0.1.20'
+depends          'sysctl'
